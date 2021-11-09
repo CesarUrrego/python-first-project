0ca4c6158cc0fc78b05db1ed6614e779daea2114
Cesar Urrego <cesar.urrego@uptc.edu.co>
2021-11-09 10:49:21 -0500

    Print in Python

---
+++ main.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+string_var = "Hola clase de Electiva Programación - Grupo 2"
+integer_var = 123
+float_var = 123.24
+
+print(string_var)
+print(type(string_var))
+
+print(integer_var)
+print(type(integer_var))
+
+print(float_var)
+print(type(float_var))
